631877075ae8707e853382685171939ba4c469bf
ChristopherParke <christopherparke@zoho.com>
2022-06-20 10:35:53 -0400

    first commit

---
+++ src/features/nft/Card.js
@@ -0,0 +1,34 @@
+import styles from "./Card.module.css";
+import equ from "../../images/image-equilibrium.jpg";
+import icon from "../../images/icon-ethereum.svg";
+import clock from "../../images/icon-clock.svg";
+import avatar from "../../images/image-avatar.png";
+
+const Card = () => {
+  return (
+    <div className={styles.card}>
+      <img className={styles.equ} src={equ} alt="" />
+      <h1 className={styles.h1}>Equilibrium #3429</h1>
+      <p>Our Equilibrium Collection promotes balance and calm.</p>
+      <div className={styles.priceWrapper}>
+        <div className={styles.ethPrice}>
+          <img src={icon} alt="" />
+          <span className={styles.eth}>0.041 ETH</span>
+        </div>
+        <div className={styles.clockContainer}>
+          <img className={styles.clock} src={clock} alt="" />
+          <span>3 days left</span>
+        </div>
+      </div>
+      <hr className={styles.hr} />
+      <div className={styles.avatarContainer}>
+        <img className={styles.avatar} src={avatar} alt="" />
+        <span>
+          Creation of <span className={styles.jules}>Jules Wyvern</span>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
