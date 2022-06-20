6d19ebdcfa8355dfd573eed30df5eb4a3aba1435
ChristopherParke <christopherparke@zoho.com>
2022-06-20 11:17:10 -0400

    added hover states

--- src/features/nft/Card.js
+++ src/features/nft/Card.js
@@ -3,11 +3,17 @@ import equ from "../../images/image-equilibrium.jpg";
 import icon from "../../images/icon-ethereum.svg";
 import clock from "../../images/icon-clock.svg";
 import avatar from "../../images/image-avatar.png";
+import eye from "../../images/icon-view.svg";
 
 const Card = () => {
   return (
     <div className={styles.card}>
-      <img className={styles.equ} src={equ} alt="" />
+      {/* <img className={styles.equ} src={equ} alt="" /> */}
+      <div className={styles.cardImage}>
+            <div class={styles.overlay}>
+                <img className={styles.eye} src={eye} alt="" />
+            </div>
+      </div>
       <h1 className={styles.h1}>Equilibrium #3429</h1>
       <p>Our Equilibrium Collection promotes balance and calm.</p>
       <div className={styles.priceWrapper}>
